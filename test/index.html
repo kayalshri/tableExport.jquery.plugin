kayalshri/tableExport.jquery.plugin

commit 40b2f4c478073a769947d14071f1f941f1e806d2

optional reforamt numbers during export
new option "numbers" that defines decimal mark and thouseads seoparator
of html input and export output. If one of them differs numbers will be
reformated during export process.

tableExport.js
@@ -37,18 +37,25 @@
         ignoreColumn: [],
         ignoreRow:[],
         jspdf: {orientation: 'p',
-          unit: 'pt',
-          format: 'a4',
-          margins: {left: 20, right: 10, top: 10, bottom: 10},
-          autotable: {padding: 2,
-            lineHeight: 12,
-            fontSize: 8,
-            tableExport: {onAfterAutotable: null,
-              onBeforeAutotable: null,
-              onTable: null
-            }
-          }
-        },
+                unit: 'pt',
+                format: 'a4',
+                margins: {left: 20, right: 10, top: 10, bottom: 10},
+                autotable: {padding: 2,
+                            lineHeight: 12,
+                            fontSize: 8,
+                            tableExport: {onAfterAutotable: null,
+                                          onBeforeAutotable: null,
+                                          onTable: null
+                                         }
+                           }
+               },
+        numbers: {html: {decimalMark: '.',
+                         thousandsSeparator: ','
+                        },
+                  output: {decimalMark: '.',
+                           thousandsSeparator: ','
+                          }
+                 },
         onCellData: null,
         outputMode: 'file', // file|string|base64
         tbodySelector: 'tr',
@@ -614,7 +621,7 @@
           teOptions.doc = null;
         }
       }
-      
+
       function ForEachVisibleCell(tableRow, selector, rowIndex, cellcallback) {
         if (defaults.ignoreRow.indexOf(rowIndex) == -1) {
           $(tableRow).filter(':visible').find(selector).each(function (colIndex) {
@@ -715,6 +722,15 @@
         return result;
       }
 
+      function parseNumber(value) {
+        value = value || "0";
+        value = replaceAll(value, defaults.numbers.html.decimalMark, '.');
+        value = replaceAll(value, defaults.numbers.html.thousandsSeparator, '');
+
+        return typeof value === "number" || jQuery.isNumeric(value) !== false ? value : false;
+      }
+
+
       function parseString(cell, rowIndex, colIndex) {
         var result = '';
 
@@ -723,8 +739,25 @@
 
           if (defaults.htmlContent === true) {
             result = $cell.html().trim();
-          } else {
+          }
+          else {
             result = $cell.text().trim().replace(/\u00AD/g, ""); // remove soft hyphens
+
+            if (defaults.numbers.html.decimalMark != defaults.numbers.output.decimalMark ||
+                defaults.numbers.html.thousandsSeparator != defaults.numbers.output.thousandsSeparator) {
+              var number = parseNumber (result);
+
+              if ( number !== false ) {
+                var frac = ("" + number).split('.');
+                if ( frac.length == 1 )
+                  frac[1] = "";
+                var mod = frac[0].length > 3 ? frac[0].length % 3 : 0;
+
+                result = (number < 0 ? "-" : "") +
+                         (defaults.numbers.output.thousandsSeparator ? ((mod ? frac[0].substr(0, mod) + defaults.numbers.output.thousandsSeparator : "") + frac[0].substr(mod).replace(/(\d{3})(?=\d)/g, "$1" + defaults.numbers.output.thousandsSeparator)) : frac[0]) +
+                         (frac[1].length ? defaults.numbers.output.decimalMark + frac[1] : "");
+              }
+            }
           }
 
           if (defaults.escape === true) {

test/index.html
@@ -128,21 +128,21 @@
       </thead>
       <tbody>
       <tr>
-        <td>1</td>
+        <td>1.101</td>
         <td>2</td>
-        <td>3</td>
+        <td>30,000</td>
       </tr>
       <tr>
         <td rowspan="3">1</td>
-        <td>2</td>
-        <td>3</td>
+        <td>.2</td>
+        <td>30000</td>
       </tr>
       <tr>
-        <td rowspan="3">2</td>
-        <td>3</td>
+        <td rowspan="3">2.2222222222</td>
+        <td>3.333</td>
       </tr>
       <tr>
-        <td>3</td>
+        <td>30,000.30</td>
       </tr>
       <tr>
         <td>1</td>

commit 154d6a8815556108cffd1154e1be5c9652d11bd4

Fixing negative number export
If we provide decimalMark and/or thousandsSeparator, the logic to
reformat the number adds an extra "-" character to negative numbers.

This bug was likely introduced in 40b2f4c478073a769947d14071f1f941f1e806d2

I've tested my change works for my use-case.
I have not tested any other use-case.

tableExport.js
@@ -1763,8 +1763,7 @@
                   frac[1] = "";
                 var mod = frac[0].length > 3 ? frac[0].length % 3 : 0;
 
-                result = (number < 0 ? "-" : "") +
-                         (defaults.numbers.output.thousandsSeparator ? ((mod ? frac[0].substr(0, mod) + defaults.numbers.output.thousandsSeparator : "") + frac[0].substr(mod).replace(/(\d{3})(?=\d)/g, "$1" + defaults.numbers.output.thousandsSeparator)) : frac[0]) +
+                result = (defaults.numbers.output.thousandsSeparator ? ((mod ? frac[0].substr(0, mod) + defaults.numbers.output.thousandsSeparator : "") + frac[0].substr(mod).replace(/(\d{3})(?=\d)/g, "$1" + defaults.numbers.output.thousandsSeparator)) : frac[0]) +
                          (frac[1].length ? defaults.numbers.output.decimalMark + frac[1] : "");
               }
             }

test/index.html
@@ -144,6 +144,7 @@
       <li><a href="#" onClick="doExport('#invisiblecols', {type: 'csv'});"> <img src='icons/csv.png' alt="CSV" style="width:24px"> CSV (data-tableexport-display="always")</a></li>
       <li><a href="#" onClick="doExport('#issue60', {type: 'csv'});"> <img src='icons/csv.png' alt="CSV" style="width:24px"> CSV (data-tableexport-value)</a></li>
       <li><a href="#" onClick="doExport('#issue127', {type: 'csv'});"> <img src='icons/csv.png' alt="CSV" style="width:24px"> CSV (issue127: Chinese csv export)</a></li>
+      <li><a href="#" onClick="doExport('#issue178', {type: 'csv', numbers: {html: {decimalMark: ',', thousandsSeparator: '.'}, output: {decimalMark: '.', thousandsSeparator: ''}}});"> <img src='icons/csv.png' alt="CSV" style="width:24px"> CSV (issue 178: negative numbers)</a></li>
       <br>
       <li><a href="#" onClick="doExport('#countries', {type: 'tsv'});"> <img src='icons/txt.png' alt="TSV" style="width:24px"> TSV</a></li>
       <br>
@@ -5741,6 +5742,29 @@
       </tr>
       </tbody>
       <tfoot></tfoot>
+    </table>
+    <h3>Issue 178: Export String to excel with rows formatted as text</h3>
+    <table id="issue178" class="table-striped">
+      <thead>
+      <tr>
+        <th>Number</th>
+        <th>Number</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>100</td>
+        <td>-100</td>
+      </tr>
+      <tr>
+        <td>1.000</td>
+        <td>-1.000</td>
+      </tr>
+      <tr>
+        <td>1.000,50</td>
+        <td>-1.000,50</td>
+      </tr>
+      </tbody>
     </table>
   </section>
 </body>